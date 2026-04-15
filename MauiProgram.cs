442e9bc286645c018be43bcc9e80e754842e8bfe
goradef <efimovsasha2007@gmail.com>
2026-04-15 21:33:59 +0300

    [new lib] added community toolkit for Toast

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,9 +1,10 @@
 ﻿using KapyTask.Database;
 using Microsoft.Extensions.Logging;
 using HorusStudio.Maui.MaterialDesignControls;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui;
 
 namespace KapyTask;
-
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
@@ -11,24 +12,17 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .UseMaterialDesignControls(options =>
-            {
-                // options.ConfigureFontSizeFromResources();
-                // options.OnException((ex) =>
-                // {
-                // });
-            })
+            .UseMauiCommunityToolkitCore()
+            .UseMaterialDesignControls()
             .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        }).UseMauiCommunityToolkit();
         builder.Services.AddSingleton<KapyTaskDatabase>();
-
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
-
         return builder.Build();
     }
 }
